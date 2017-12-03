ba88ff3bb90990eaec8662eebec74853b02ad894
Kenrick Hatton <Kenrick.hatton@gmail.com>
2017-12-03 17:09:35 +0000

    Api Implimintaion

--- js/brdApp.js
+++ js/brdApp.js
@@ -1,4 +1,4 @@
-angular.module("MyApp", ['ngRoute', 'RouteControllers']);
+angular.module("MyApp", ['ngRoute', 'RouteControllers', 'UserService', 'angular-storage','RegisterService', 'RegDirective']);
 
 angular.module("MyApp").config(function($locationProvider, $routeProvider) {
 	$locationProvider.html5Mode(true); //Enable href routing without hashes
@@ -14,5 +14,13 @@ angular.module("MyApp").config(function($locationProvider, $routeProvider) {
 	.when('/Suppliers',{
 		templateUrl: '/S1_templates/brdSuppliers.html',
 		controller: 'SupplierController'
+	})
+	.when('/BeRaw/register', {
+		templateUrl:'/S1_templates/brdRegister.html',
+		controller: 'RegisterController'
+	})
+	.when('/BeRaw/login/', {
+		templateUrl: '/S1_templates/brdLogin.html',
+		controller: 'LoginController'
 	});
 });
